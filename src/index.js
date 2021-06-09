5af104748ec4b8f0619a666d64b90274e12e4e75
mAnic <arctic.manic@gmail.com>
2021-06-09 21:55:24 +0300

    Remove react router

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import rootReducer from './store/root-reducer';
 import { createApi } from './api/api';
-import browserHistory from './browser-history';
 import { getServers } from './store/data/actions';
-import { Router } from 'react-router-dom';
 import { data } from './mock/data';
 
 import './index.css';
@@ -27,9 +25,7 @@ store.dispatch(getServers(data));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
